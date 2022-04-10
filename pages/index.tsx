e7d91ae2b54594f3b91bcfbe75e9c5b57c2330d6
Maduki-tech <maduki.business@gmail.com>
2022-04-10 21:56:27 +0200

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,23 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <div >
+    <div>
       <Head>
         <title>Hulu 2.1</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-				<h1>Test</h1>
+      <h1>Test</h1>
 
+      {
+        // Header
+        // Nav
+        // Results
+      }
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
